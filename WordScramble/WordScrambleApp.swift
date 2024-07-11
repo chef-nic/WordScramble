7c3742fd5507126c508de7a85a1e02cee09a9c5b
chef-nic <nicholasajohnson@me.com>
2024-07-11 09:31:36 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nicholas Johnson on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
